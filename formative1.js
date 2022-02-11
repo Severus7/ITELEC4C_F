03ab58f2c4714460d6d9f258fe120794aa0189fa
theBruller <jcaquino0945@gmail.com>
2022-02-11 19:47:26 +0800

    round1

--- formative1.js
+++ formative1.js
@@ -1,9 +1,37 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const prompt = require('prompt');
 
-let card1 = Math.floor(Math.random() * 13 + 1);
-let card2 = Math.floor(Math.random() * 13 + 1);
-let card3 = Math.floor(Math.random() * 13 + 1);
+let rounds = 5;
+let answers = [];
+
+function round1() {
+  prompt.start();
+  let card1 = Math.floor(Math.random() * 13 + 1);
+  let card2 = Math.floor(Math.random() * 13 + 1);
+
+  console.log(`Card 1: ${card1}`);
+  console.log(`Card 2: ${card2}`);
+
+  if (card1 != card2) {
+    let card3 = Math.floor(Math.random() * 13 + 1);
+    console.log('DEAL OR NO DEAL');
+    prompt.get('offer', function (err,result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if ((result.offer == 'DEAL') && ((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+      }
+      else if ((result.offer == 'NO DEAL') && !((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log('talo ka')
+      }
+    })
+  }
+}
+round1();
+
--- test.js
+++ test.js
@@ -1,20 +1,3 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('What is your name ? ', function (name) {
-  rl.question('Where do you live ? ', function (country) {
-    console.log(`${name}, is a citizen of ${country}`);
-    rl.close();
-  });
-});
-
-rl.on('close', function () {
-  console.log('\nBYE BYE !!!');
-  process.exit(0);
-});
 
 let card1 = Math.floor(Math.random() * 13 + 1);
 let card2 = Math.floor(Math.random() * 13 + 1);
@@ -34,4 +17,3 @@ if ((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2)) {
 } else {
   console.log("NOT");
 }
-
