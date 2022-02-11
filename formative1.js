3f5d8a80558128271cfbffe188688e8a5323aca2
theBruller <jcaquino0945@gmail.com>
2022-02-11 20:33:26 +0800

    Working

--- formative1.js
+++ formative1.js
@@ -1,20 +1,20 @@
 const prompt = require('prompt');
 
 let rounds = 5;
-let answers = [];
+let score = 0;
 
-function round1() {
+function commence() {
   prompt.start();
   let card1 = Math.floor(Math.random() * 13 + 1);
   let card2 = Math.floor(Math.random() * 13 + 1);
+  let card3 = Math.floor(Math.random() * 13 + 1);
 
   console.log(`Card 1: ${card1}`);
   console.log(`Card 2: ${card2}`);
 
   if (card1 != card2) {
-    let card3 = Math.floor(Math.random() * 13 + 1);
     console.log('DEAL OR NO DEAL');
-    prompt.get('offer', function (err,result) {
+    prompt.get('offer', function (err, result) {
       if (err) {
         return onErr(err);
       }
@@ -22,16 +22,287 @@ function round1() {
       if ((result.offer == 'DEAL') && ((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
         console.log(`Card 3: ${card3}`);
         console.log('PANALO KA')
+        score++;
+        round2()
+      }
+      else if ((result.offer == 'NO DEAL') && !((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA');
+        score++;
+        round2();
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log('talo ka')
+        round2()
+      }
+    })
+  }
+  else if (card1 === card2) {
+    console.log("HIGHER or LOWER");
+    prompt.get("offer", function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if (result.offer == "HIGHER" && card3 > card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        round2()
+      } else if (result.offer == "LOWER" && card3 < card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        round2()
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log("Talo ka!");
+        round2()
+      }
+    });
+  }  
+}
+
+function round2() {
+  prompt.start();
+  let card1 = Math.floor(Math.random() * 13 + 1);
+  let card2 = Math.floor(Math.random() * 13 + 1);
+  let card3 = Math.floor(Math.random() * 13 + 1);
+
+  console.log(`Card 1: ${card1}`);
+  console.log(`Card 2: ${card2}`);
+
+  if (card1 != card2) {
+    console.log('DEAL OR NO DEAL');
+    prompt.get('offer', function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if ((result.offer == 'DEAL') && ((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+        score++;
+        round3()
       }
       else if ((result.offer == 'NO DEAL') && !((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
         console.log(`Card 3: ${card3}`);
         console.log('PANALO KA')
+        score++;
+        round3()
       } else {
         console.log(`Card 3: ${card3}`);
         console.log('talo ka')
+        round3()
       }
     })
   }
+  else if (card1 === card2) {
+    console.log("HIGHER or LOWER");
+    prompt.get("offer", function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if (result.offer == "HIGHER" && card3 > card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        round3()
+      } else if (result.offer == "LOWER" && card3 < card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        round3()
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log("Talo ka!");
+        round3()
+      }
+    });
+  }  
 }
-round1();
+
+function round3() {
+  prompt.start();
+  let card1 = Math.floor(Math.random() * 13 + 1);
+  let card2 = Math.floor(Math.random() * 13 + 1);
+  let card3 = Math.floor(Math.random() * 13 + 1);
+
+  console.log(`Card 1: ${card1}`);
+  console.log(`Card 2: ${card2}`);
+
+  if (card1 != card2) {
+    console.log('DEAL OR NO DEAL');
+    prompt.get('offer', function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if ((result.offer == 'DEAL') && ((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+        score++;
+        round4()
+      }
+      else if ((result.offer == 'NO DEAL') && !((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+        score++;
+        round4()
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log('talo ka')
+        round4()
+      }
+    })
+  }
+  else if (card1 === card2) {
+    console.log("HIGHER or LOWER");
+    prompt.get("offer", function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if (result.offer == "HIGHER" && card3 > card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        round4()
+      } else if (result.offer == "LOWER" && card3 < card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        round4()
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log("Talo ka!");
+        round4()
+      }
+    });
+  }  
+}
+
+function round4() {
+  prompt.start();
+  let card1 = Math.floor(Math.random() * 13 + 1);
+  let card2 = Math.floor(Math.random() * 13 + 1);
+  let card3 = Math.floor(Math.random() * 13 + 1);
+
+  console.log(`Card 1: ${card1}`);
+  console.log(`Card 2: ${card2}`);
+
+  if (card1 != card2) {
+    console.log('DEAL OR NO DEAL');
+    prompt.get('offer', function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if ((result.offer == 'DEAL') && ((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+        score++;
+        round5()
+      }
+      else if ((result.offer == 'NO DEAL') && !((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+        score++;
+        round5()
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log('talo ka')
+        round5()
+      }
+    })
+  }
+  else if (card1 === card2) {
+    console.log("HIGHER or LOWER");
+    prompt.get("offer", function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if (result.offer == "HIGHER" && card3 > card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        round5()
+      } else if (result.offer == "LOWER" && card3 < card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        round5()
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log("Talo ka!");
+        round5()
+      }
+    });
+  }  
+}
+
+function round5() {
+  prompt.start();
+  let card1 = Math.floor(Math.random() * 13 + 1);
+  let card2 = Math.floor(Math.random() * 13 + 1);
+  let card3 = Math.floor(Math.random() * 13 + 1);
+
+  console.log(`Card 1: ${card1}`);
+  console.log(`Card 2: ${card2}`);
+
+  if (card1 != card2) {
+    console.log('DEAL OR NO DEAL');
+    prompt.get('offer', function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if ((result.offer == 'DEAL') && ((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+        score++;
+        console.log('Total Points: ' + score)
+      }
+      else if ((result.offer == 'NO DEAL') && !((card3 >= card1 && card3 <= card2) || (card3 <= card1 && card3 >= card2))) {
+        console.log(`Card 3: ${card3}`);
+        console.log('PANALO KA')
+        score++;
+        console.log('Total Points: ' + score)
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log('talo ka')
+        console.log('Total Points: ' + score)
+      }
+    })
+  }
+  else if (card1 === card2) {
+    console.log("HIGHER or LOWER");
+    prompt.get("offer", function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      if (result.offer == "HIGHER" && card3 > card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        console.log('Total Points: ' + score)
+      } else if (result.offer == "LOWER" && card3 < card1) {
+        console.log(`Card 3: ${card3}`);
+        console.log("PANALO KA");
+        score++;
+        console.log('Total Points: ' + score)
+      } else {
+        console.log(`Card 3: ${card3}`);
+        console.log("Talo ka!");
+        console.log('Total Points: ' + score)
+      }
+    });
+  }  
+}
+
+
+commence();
 
